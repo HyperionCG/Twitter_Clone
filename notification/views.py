35576dc2c488b38fc852c416edc35e1edc195100
Babatunde Odumosu <tudy4300@pop-os.localdomain>
2020-07-04 13:29:40 -0500

    added more content

--- notification/views.py
+++ notification/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse, HttpResponseRedirect
+from tweet.models import Tweets
+from tweet.forms import TweetForm
+from notification.models import Notifications
+from twitteruser.models import TwitterUser
+
 
 # Create your views here.
 
-def notifications_view(request):
-    user_data=request.user
-    
-    return render(request, 'index.htm')
+def notifications_view(request, id):
+    pingeduser = TwitterUser.objects.filter(id=id).first()
+    data = Notifications.objects.filter(user=pingeduser)
+    for notice in data:
+        item.delete()
+    return render(request, 'notifications.htm', {'data': data})
